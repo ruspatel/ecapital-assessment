a4e8b2fa573abf06865c96e2fbdd37afede26faf
ra8patel <ra8patel@uwaterloo.ca>
2023-08-28 17:26:02 -0400

    generalized table component to accept props

--- frontend/src/components/Employees.js
+++ frontend/src/components/Employees.js
@@ -4,9 +4,25 @@ import Table from './Table';
 
 function Employees() {
 
+    const headings = [
+        { name: "First Name", id : "first_name" },
+        { name: "Last Name", id: "last_name" },
+        { name: "Salary", id: "salary"}
+    ];
+
+    const employeeData = [
+        {"first_name": "Jane", "last_name": "Doe", "salary": "$99,000"},
+        {"first_name": "Ian", "last_name": "Malcolm", "salary": "$99,000"},
+        {"first_name": "Ellie", "last_name": "Sattler", "salary": "$99,000"},
+        {"first_name": "Dennis", "last_name": "Nedry", "salary": "$99,000"},
+    ];
+
     return(
         <div>
-            <Table/>
+            <Table
+                headings={headings}
+                data={employeeData}
+            />
         </div>
     );
 }
--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,62 +1,36 @@
 import React from 'react';
 
-function Table() {
+function Table(
+    {
+        headings,
+        data
+    }
+) {
 
     return(
         <div>
             <table>
                 <thead>
                     <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Salary</th>
+                        {headings.map((heading) => {
+                            return(
+                                <th>{heading.name}</th>
+                            )
+                        })}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            Lewis
-                        </td>
-                        <td>
-                            Burson
-                        </td>
-                        <td>
-                            $99,000
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Ian
-                        </td>
-                        <td>
-                            Malcolm
-                        </td>
-                        <td>
-                            $99,000
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Ellie
-                        </td>
-                        <td>
-                            Sattler
-                        </td>
-                        <td>
-                            $99,000
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Dennis
-                        </td>
-                        <td>
-                            Nedry
-                        </td>
-                        <td>
-                            $99,000
-                        </td>
-                    </tr>
+                    {data.map((dataRow) => {
+                        return(
+                            <tr>
+                                {headings.map((heading) => {
+                                    return(
+                                        <td>{dataRow[heading.id]}</td> 
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </div>
