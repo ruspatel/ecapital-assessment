c4a6ed4662486f080261f5bae877522e8b2ab9c6
ra8patel <ra8patel@uwaterloo.ca>
2023-08-28 21:38:04 -0400

    added gitignore and employee model

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Employee Model
+class Employee(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    salary = models.IntegerField()
