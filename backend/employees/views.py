e4b927f53a2f88bcf8338b37ee29ea50247cc706
ra8patel <ra8patel@uwaterloo.ca>
2023-08-28 22:02:30 -0400

    added get employees list endpoint

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Employee
+from .serializers import EmployeeSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+# GET endpoint for obtaining a list of employees
+@api_view(['GET'])
+def get_employees_list(_):
+    grocery_lists = Employee.objects.all()
+    serializer = EmployeeSerializer(grocery_lists, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
