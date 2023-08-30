ec379bcd68a96a898679c3fa14610c8d2584f12c
ra8patel <ra8patel@uwaterloo.ca>
2023-08-29 20:22:28 -0400

    added allowed hosts to settings file

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*lo93yu7kge)y4xhna!53=bwvly0#6ttw!m8w6kq1%o_xlrd04
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
